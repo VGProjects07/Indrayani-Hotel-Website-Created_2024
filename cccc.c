bcff3169c3471dda70a7657a889779a300e40db2
Vishal's Projects <vishalganbote07@gmail.com>
2025-10-23 20:46:06 +0530

    Add files via upload

---
+++ cccc.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include<string.h>
+int main(){
+    char ch[50];
+    scanf("%s",ch);
+    printf("%c",ch[0]);
+    printf("%c",ch[1]);
+    printf("%c",ch[2]);
+    return 0;
+}
